abdc7136b3b02ff66de82579755933ce12316492
S.Paulino <paulino.samuel@yahoo.com>
2014-02-23 15:16:11 -0500

    Control/debug fixes
    -changed bat model
    -added textures
    -added motion trail
    -added pitch selection

--- New Unity Project/Assets/Scripts/PitchScript.cs
+++ New Unity Project/Assets/Scripts/PitchScript.cs
@@ -6,11 +6,13 @@ public class PitchScript : MonoBehaviour {
 	public GameObject baseball;
 	private GameObject ball;
 	private Vector3 releasePos;
-	public Vector3 releaseVelocity;
+	public Vector3 releaseVelocity; //in meters per second
 	public Vector3 curve;
 
 	public enum pitchType {Fastball, Sinker, Splitter, Curveball, Slider, Screwball, Changeup, Forkball, Cutter, Slurve, Palmball, CircleChangeup};
 	public pitchType pitch;
+
+	private int mph;
 
 	// Use this for initialization
 	void Start () {
@@ -20,10 +22,19 @@ public class PitchScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.anyKeyDown) {
-						ball = GameObject.Instantiate (baseball, 
-			                               			   releasePos, 
-			                               			   Quaternion.identity) as GameObject;
+		if (Input.GetKeyDown(KeyCode.RightArrow)) {
+			if( pitch != pitchType.CircleChangeup)
+				pitch += 1;
+			else
+				pitch = pitchType.Fastball;
+		} 
+		else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+			if( pitch != pitchType.Fastball)
+				pitch -= 1;
+			else
+				pitch = pitchType.CircleChangeup;
+		}
+		else if (Input.GetButtonDown("Jump")) {
 
 			switch (pitch) { //right now all pitches go at their max speed 
 			case pitchType.Fastball:
@@ -32,15 +43,15 @@ public class PitchScript : MonoBehaviour {
 				break;
 			case pitchType.Sinker:
 				releaseVelocity.Set(40.23f, 5, 0); // 80-90 mph
-				curve = new Vector3(0, -0.5f, -0.5f);
+				curve = new Vector3(0, -3, -0.5f);
 				break;
 			case pitchType.Splitter:
 				releaseVelocity.Set(40.23f, 10, 0); // 80-90 mph
 				curve = new Vector3(0, 0, 0);
 				break;
 			case pitchType.Curveball:
-				releaseVelocity.Set(35.76f, 13, 0); // 70-80 mph
-				curve = new Vector3(0, 0, 0);
+				releaseVelocity.Set(35.76f, 11, 0); // 70-80 mph
+				curve = new Vector3(0, -5, 0);
 				break;
 			case pitchType.Slider:
 				releaseVelocity.Set(40.23f, 5, 0); // 80-90 mph
@@ -48,7 +59,7 @@ public class PitchScript : MonoBehaviour {
 				break;
 			case pitchType.Screwball:
 				releaseVelocity.Set(33.53f, 12, 0); // 65-75 mph
-				curve = new Vector3(0, 0, 0);
+				curve = new Vector3(0, -5, -0.5f);
 				break;
 			case pitchType.Changeup:
 				releaseVelocity.Set(38.0f, 10, 0); // 70-85 mph
@@ -59,12 +70,12 @@ public class PitchScript : MonoBehaviour {
 				curve = new Vector3(0, 0, 0);
 				break;
 			case pitchType.Cutter:
-				releaseVelocity.Set(42.47f, 3, 0); // 85-95 mph
-				curve = new Vector3(0, 0, 0);
+				releaseVelocity.Set(42.47f, 1, 0); // 85-95 mph
+				curve = new Vector3(0, 0, 0.5f);
 				break;
 			case pitchType.Slurve:
-				releaseVelocity.Set(35.76f, 12, 0); // 70-80 mph
-				curve = new Vector3(0, 0, 0);
+				releaseVelocity.Set(35.76f, 11, 0); // 70-80 mph
+				curve = new Vector3(0, -5, 0.5f);
 				break;
 			case pitchType.Palmball:
 				releaseVelocity.Set(33.53f, 12, 0); // 65-75 mph
@@ -81,13 +92,30 @@ public class PitchScript : MonoBehaviour {
 				break;
 			}
 
+			ball = GameObject.Instantiate (baseball, 
+			                               releasePos, 
+			                               Quaternion.identity) as GameObject;
 			ball.rigidbody.velocity = releaseVelocity; //speed vector in meters per second
 			ball.rigidbody.AddTorque (curve);
 				}
+
+		//change camera
+		if (Input.GetButtonDown ("Fire1")) {
+
+			}
 	}
 
 	void FixedUpdate () {
 		if (ball != null)
 			ball.rigidbody.AddForce(curve);
 	}
+
+	void OnGUI () {
+		//convert to mph
+		mph = (int) ((releaseVelocity.x / 1609.34f) * 3600);
+
+		GUI.BeginGroup (new Rect (10, 10, 1000, 100));
+		GUI.Label(new Rect (0, 0, 200, 100), "Last Pitch: " + pitch.ToString()+ " at " + mph + " MPH");
+		GUI.EndGroup ();
+		}
 }
