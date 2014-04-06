b0c12d2963aebf349a787cbd1b2ccf0d9a7381d5
S.Paulino <paulino.samuel@yahoo.com>
2014-04-06 15:52:57 -0400

    Rift integration
    -iimported the oculus vr plugin
    - built upon bat and swing scripts

--- New Unity Project/Assets/Scripts/BatScript.cs
+++ New Unity Project/Assets/Scripts/BatScript.cs
@@ -6,18 +6,22 @@ public class BatScript : MonoBehaviour {
 	private bool fCollided = false;
 	private Vector3 fLastVel;
 	private Collision fCollision;
+	private Vector3 pivotOffset;
 
 	public float bbcor = 0.25f; // Ball-Bat Coefficient of Restitution
 	private Vector3 bbs; //Batted Ball Speed
+	
 
 	// Use this for initialization
 	void Start () {
-	
+			pivotOffset = new Vector3 (0, 0, -0.45f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//transform.Rotate (0, -20, 0);
+		//transform.Rotate (0, -2, 0);
+
+
 	}
 
 	void FixedUpdate(){
@@ -25,8 +29,15 @@ public class BatScript : MonoBehaviour {
 			fCollided = false; // reset flag
 			// calculate speed due to collision
 			Vector3 pitchSpeed = fCollision.rigidbody.velocity;
-			Vector3 batSpeed = rigidbody.velocity;
+
+			Vector3 pivot = transform.position + pivotOffset;
+
+			//approx. radius from point of contact to pivot
+			float r = Vector3.Distance(pivot, fCollision.contacts[0].point); 
+			Vector3 batSpeed = rigidbody.velocity + (rigidbody.angularVelocity * r);
 			bbs = bbcor *(pitchSpeed) + (1+bbcor)*(batSpeed);
+
+			Debug.Log("pitch speed: " + pitchSpeed + "bat speed: " + batSpeed);
 
 			// call OnAfterCollision passing the Collision 
 			// info and the reaction force:
@@ -48,7 +59,7 @@ public class BatScript : MonoBehaviour {
 	}
 
 	void  OnAfterCollision(Collision coll, Vector3 speed){
-		//assign speed to ball
-		coll.rigidbody.velocity = speed;
+		//assign speed to ball in the direction of the contact normal
+		//coll.rigidbody.velocity = speed * coll.contacts[0].normal;
 	}
 }
---
+++ New Unity Project/Assets/Scripts/SwingScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwingScript : MonoBehaviour {
+
+	public float speed;
+	private Vector3 startPos;
+	private Quaternion startRot;
+	private Quaternion endRot;
+	private bool isSwinging = false;
+
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+		startRot = transform.rotation;
+		endRot.eulerAngles = new Vector3 (0,-100,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (isSwinging) {
+			transform.rotation = Quaternion.Lerp (startRot, endRot, Time.time * -speed);
+			transform.Translate(-5 * Time.deltaTime, 0, 0);
+
+			if (transform.position.x <= (startPos.x - 1) ) {
+				isSwinging = false;
+				transform.position = startPos;
+				Debug.Log("stopped");
+			}
+			/*if (transform.rotation == endRot){
+				isSwinging = false;
+				rigidbody.angularVelocity = new Vector3(0,0,0);
+				transform.rotation = startRot;
+				Debug.Log("stopped");
+			}
+			*/
+		} else {
+			if (Input.GetKeyDown ("s")){
+				//rigidbody.AddTorque(0, speed, 0);
+				isSwinging = true;
+				Debug.Log("isSwinging");
+			}
+		}
+
+	}
+
+	void Swing () {
+
+	}
+}
